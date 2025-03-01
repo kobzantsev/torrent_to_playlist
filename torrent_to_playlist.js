1efd4e66b8752f1763a22bbcdf780a0704679302
Max <21959445+kobzantsev@users.noreply.github.com>
2025-03-01 09:44:17 +0200

    Update torrent_to_playlist.js

--- torrent_to_playlist.js
+++ torrent_to_playlist.js
@@ -1,11 +1,5 @@
 (function() {
     'use strict';
 
-    Lampa.Plugin.register({
-        name: 'TorrentToPlaylist',
-        version: '1.0.1',
-        description: 'Тестовый плагин'
-    });
-
     Lampa.Noty.show('Плагин успешно загружен!');
 })();
